1526b74b4c05e39fe015ce9b4c674b4b967efd9a
omkarpg0931 <omkar0931@gmail.com>
2015-06-24 15:55:15 +0530

    Version 1

--- Elevator/src/elevatorPgm/Elevator.java
+++ Elevator/src/elevatorPgm/Elevator.java
@@ -2,4 +2,29 @@ package elevatorPgm;
 
 public class Elevator {
 
+	public void openDoors() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void goToFloor(int u) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void goingUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void print(int a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void closeDoors() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- Elevator/src/elevatorPgm/ElevatorMain.java
+++ Elevator/src/elevatorPgm/ElevatorMain.java
@@ -2,9 +2,21 @@ package elevatorPgm;
 
 public class ElevatorMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
+		Elevator ob=new Elevator();
+		Elevator obdup=new Elevator();
+		ob.openDoors();
+		System.out.print("\nPresent floor=");
+		int a= 1;
+		System.out.print("\nDesired floor=");
+		int b= 0;
+		ob.goToFloor(b);
 
+		obdup.goingUp();
+		obdup.goingUp();
+		ob.print(a);
+		obdup.closeDoors();
 	}
 
 }
